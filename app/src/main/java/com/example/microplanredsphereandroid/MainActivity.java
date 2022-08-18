8f512b9b70bb5bff918a4b58c6e6074f29e06921
Pridemore.Vhiriri <prideviriri>
2022-08-18 22:23:27 +0200

    creating splash screen and link it to the login

--- app/src/main/java/com/example/microplanredsphereandroid/MainActivity.java
+++ app/src/main/java/com/example/microplanredsphereandroid/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.microplanredsphereandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      Intent i=new Intent();
     }
 }
