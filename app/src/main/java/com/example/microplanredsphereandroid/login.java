cd31cdd587f5b1ce3b619dce6c4d7f97d7d9333b
Pridemore.Vhiriri <prideviriri>
2022-08-18 23:17:53 +0200

    link login to the MainActivity

--- app/src/main/java/com/example/microplanredsphereandroid/login.java
+++ app/src/main/java/com/example/microplanredsphereandroid/login.java
@@ -2,13 +2,29 @@ package com.example.microplanredsphereandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
-
+Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login=findViewById(R.id.sign_in);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(com.example.microplanredsphereandroid.login.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
+
 }
