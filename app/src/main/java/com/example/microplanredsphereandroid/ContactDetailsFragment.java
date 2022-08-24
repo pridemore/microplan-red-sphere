0d782eedae37af0f7e639039d9dbe811af9f3329
Pridemore.Vhiriri <prideviriri>
2022-08-24 23:44:27 +0200

    adding ContactDetailsFragment. Adding logic to previous and next buttons for Contact details.

--- app/src/main/java/com/example/microplanredsphereandroid/ContactDetailsFragment.java
+++ app/src/main/java/com/example/microplanredsphereandroid/ContactDetailsFragment.java
@@ -1,6 +1,5 @@
 package com.example.microplanredsphereandroid;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,8 +11,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PersonalDetailsFragment extends Fragment {
-    private static final String TAG = "Personal Details";
+public class ContactDetailsFragment extends Fragment {
+    private static final String TAG = "Contact Details";
     ImageView backIcon;
     ImageView menu;
     TextView title;
@@ -22,32 +21,23 @@ public class PersonalDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_personal_details, container, false);
+        View view= inflater.inflate(R.layout.fragment_contact_details, container, false);
         backIcon=view.findViewById(R.id.left_icon);
         menu=view.findViewById(R.id.right_icon);
         title=view.findViewById(R.id.title);
         btn_previous=view.findViewById(R.id.btn_previous);
         btn_nxt=view.findViewById(R.id.btn_nxt);
-        title.setText("Personal Details");
+        title.setText("Contact Details");
 
         btn_previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((NewApplicationActivity)getActivity()).replaceFragment(new NewApplicationFragment());
+                ((NewApplicationActivity)getActivity()).replaceFragment(new PersonalDetailsFragment());
 //                Intent intent=new Intent(getActivity(),HomeActivity.class);
 //                startActivity(intent);
             }
         });
 
-        btn_nxt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ((NewApplicationActivity)getActivity()).replaceFragment(new ContactDetailsFragment());
-            }
-        });
-
-
         return view;
     }
-
 }
