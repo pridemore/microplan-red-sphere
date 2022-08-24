26e1ef008f533ecd5fe73a14139bfcc6b8eee305
Pridemore.Vhiriri <prideviriri>
2022-08-25 00:01:15 +0200

    Adding Employement,BankDetails,NxtKin1 and NxtKin2 Fragments         Adding logic for all Previous and Next buttons on the fragments

---
+++ app/src/main/java/com/example/microplanredsphereandroid/EmploymentFragment.java
@@ -0,0 +1,52 @@
+package com.example.microplanredsphereandroid;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EmploymentFragment extends Fragment {
+    private static final String TAG = "Employment";
+    ImageView backIcon;
+    ImageView menu;
+    TextView title;
+    Button btn_previous, btn_nxt;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_employment, container, false);
+        backIcon=view.findViewById(R.id.left_icon);
+        menu=view.findViewById(R.id.right_icon);
+        title=view.findViewById(R.id.title);
+        btn_previous=view.findViewById(R.id.btn_previous);
+        btn_nxt=view.findViewById(R.id.btn_nxt);
+        title.setText("Employment");
+
+        btn_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((NewApplicationActivity)getActivity()).replaceFragment(new ContactDetailsFragment());
+//                Intent intent=new Intent(getActivity(),HomeActivity.class);
+//                startActivity(intent);
+            }
+        });
+
+        btn_nxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((NewApplicationActivity)getActivity()).replaceFragment(new BankDetailsFragment());
+            }
+        });
+
+
+
+        return view;
+    }
+}
