a99a42062aaef9144446fc5356ed59ed3faf944c
Pridemore.Vhiriri <prideviriri>
2022-08-27 18:24:47 +0200

    personal details page interface done.

--- app/src/main/java/com/example/microplanredsphereandroid/PersonalDetailsFragment.java
+++ app/src/main/java/com/example/microplanredsphereandroid/PersonalDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.example.microplanredsphereandroid;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -9,15 +10,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class PersonalDetailsFragment extends Fragment {
     private static final String TAG = "Personal Details";
+    final Calendar myCalendar= Calendar.getInstance();
     ImageView backIcon;
     ImageView menu;
     TextView title;
     Button btn_previous, btn_nxt;
+    private TextInputEditText dob;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,6 +38,7 @@ public class PersonalDetailsFragment extends Fragment {
         title=view.findViewById(R.id.title);
         btn_previous=view.findViewById(R.id.btn_previous);
         btn_nxt=view.findViewById(R.id.btn_nxt);
+        dob=view.findViewById(R.id.dob);
         title.setText("Personal Details");
 
         btn_previous.setOnClickListener(new View.OnClickListener() {
@@ -45,9 +56,28 @@ public class PersonalDetailsFragment extends Fragment {
                 ((NewApplicationActivity)getActivity()).replaceFragment(new ContactDetailsFragment());
             }
         });
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(getActivity(),date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
 
         return view;
     }
-
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        dob.setText(dateFormat.format(myCalendar.getTime()));
+    }
 }
