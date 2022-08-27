0c744b3c027ee2bd60f9764800269e3004bf0e8c
Pridemore.Vhiriri <prideviriri>
2022-08-27 23:04:18 +0200

    Loan details pages interface done.

--- app/src/main/java/com/example/microplanredsphereandroid/LoanDetailsFragment.java
+++ app/src/main/java/com/example/microplanredsphereandroid/LoanDetailsFragment.java
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class LoanDetailsFragment extends Fragment {
@@ -17,6 +19,7 @@ public class LoanDetailsFragment extends Fragment {
     ImageView menu;
     TextView title;
     Button btn_previous, btn_nxt;
+    Spinner repaymentPeriodRequired,facilityType;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class LoanDetailsFragment extends Fragment {
         title=view.findViewById(R.id.title);
         btn_previous=view.findViewById(R.id.btn_previous);
         btn_nxt=view.findViewById(R.id.btn_nxt);
+        repaymentPeriodRequired=view.findViewById(R.id.repaymentPeriodRequired);
+        facilityType=view.findViewById(R.id.facilityType);
         title.setText("Loan Details");
 
         btn_previous.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +49,16 @@ public class LoanDetailsFragment extends Fragment {
                 ((NewApplicationActivity)getActivity()).replaceFragment(new BankDetailsFragment());
             }
         });
+
+        ArrayAdapter<CharSequence>adapterRepaymentPeriod=ArrayAdapter.createFromResource(getActivity(),
+                R.array.repayment_period, android.R.layout.simple_spinner_item);
+        adapterRepaymentPeriod.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        repaymentPeriodRequired.setAdapter(adapterRepaymentPeriod);
+
+        ArrayAdapter<CharSequence>adapterFacilityType=ArrayAdapter.createFromResource(getActivity(),
+                R.array.facility_type, android.R.layout.simple_spinner_item);
+        adapterFacilityType.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        facilityType.setAdapter(adapterFacilityType);
 
 
 
