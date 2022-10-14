c7e05a967a423d5fb69cf55bf2cdedee9c32a975
Pridemore.Vhiriri <prideviriri>
2022-10-14 07:39:34 +0200

    Added code cleaning on LoanDetails

--- app/src/main/java/com/example/microplanredsphereandroid/LoanDetailsFragment.java
+++ app/src/main/java/com/example/microplanredsphereandroid/LoanDetailsFragment.java
@@ -1,17 +1,18 @@
 package com.example.microplanredsphereandroid;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Calendar;
 
 public class LoanDetailsFragment extends Fragment {
     private static final String TAG = "Loan Details";
@@ -19,34 +20,42 @@ public class LoanDetailsFragment extends Fragment {
     ImageView menu;
     TextView title;
     Button btn_previous, btn_nxt;
+    private TextInputEditText dob, netSalary, loanPurpose, loanPeriod;
+    final Calendar myCalendar = Calendar.getInstance();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_loan_details, container, false);
-        backIcon=view.findViewById(R.id.left_icon);
-        menu=view.findViewById(R.id.right_icon);
-        title=view.findViewById(R.id.title);
-        btn_previous=view.findViewById(R.id.btn_previous);
-        btn_nxt=view.findViewById(R.id.btn_nxt);
+        View view = inflater.inflate(R.layout.fragment_loan_details, container, false);
+
+        //instantiating views
+        backIcon = view.findViewById(R.id.left_icon);
+        menu = view.findViewById(R.id.right_icon);
+        title = view.findViewById(R.id.title);
+        btn_previous = view.findViewById(R.id.btn_previous);
+        btn_nxt = view.findViewById(R.id.btn_nxt);
+        dob = view.findViewById(R.id.dob);
+        netSalary = view.findViewById(R.id.netSalary);
+        loanPurpose = view.findViewById(R.id.loanPurpose);
+        loanPeriod = view.findViewById(R.id.loanPeriod);
+
+        //setting static text
         title.setText("Loan Details");
 
+        //Buttons logic
         btn_previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((NewApplicationActivity)getActivity()).replaceFragment(new Products());
-//                Intent intent=new Intent(getActivity(),HomeActivity.class);
-//                startActivity(intent);
+                ((NewApplicationActivity) getActivity()).replaceFragment(new Products());
             }
         });
-
         btn_nxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ((NewApplicationActivity)getActivity()).replaceFragment(new PersonalDetailsFragment());
+                ((NewApplicationActivity) getActivity()).replaceFragment(new PersonalDetailsFragment());
             }
         });
-
 
 
         return view;
