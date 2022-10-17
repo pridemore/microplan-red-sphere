b87f2292eaea5b987305415d7b0f1bd6f319fb47
Pridemore.Vhiriri <prideviriri>
2022-10-17 04:11:09 +0200

    Added fields to new model LoanApplications

--- app/src/main/java/com/example/microplanredsphereandroid/models/LoanApplications.java
+++ app/src/main/java/com/example/microplanredsphereandroid/models/LoanApplications.java
@@ -1,57 +1,150 @@
 package com.example.microplanredsphereandroid.models;
 
 public class LoanApplications {
-    private String name;
-    private String surname;
-    private int installment;
-    private String product ;
-    private String applicationDate;
+    public String uniqueRef;
+    public String application_title;
+    public long agent_id = 7;
+    public long id;
+    public String isSubmitted = "false";
+    private String dateAndTime;
 
-    public LoanApplications(String name, String surname, int installment, String product, String applicationDate) {
-        this.name = name;
-        this.surname = surname;
-        this.installment = installment;
-        this.product = product;
-        this.applicationDate = applicationDate;
-    }
+    //Loan Details
+    public int loanPeriod;
+    public double netSalary;
+    private String loanPurpose;
+    private String approvedLoanAmount;
+    private String establishmentFees;
+    private String fiftyPercentOfSalary;
+    private String loanApplicationFee;
+    private String loanInsuranceFees;
+    private String loanRepaymentPerMonth;
+    private String loanType;
+    private String fundsTransferFees;
+    private String interestRate;
+    private String newLoanAmount;
+    private String price;
+    private String loanFromDate;
+    private String loanToDate;
 
-    public String getName() {
-        return name;
-    }
+    //Personal
+    private String title;
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String nationalId;
+    private String passportNumber;
+    private String maidenName;
+    private String countryOfBirth;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    //Contact Details
+    public String residentialAddress;
+    private String addressType;
+    private String mobileNumber;
+    private String email;
+    private String currentCitizenship;
 
-    public String getSurname() {
-        return surname;
-    }
+    //Employment Details
+    private String profession;
+    private String nameOfEmployer;
+    private String otherNameOfEmployer;
+    private String employerPhysicalAddress;
+    private String positionHeld;
+    private String employeeNumber;
+    private String expiryOfEmployment;
+    private String dateJoined;
+    private String grossSalary;
+    private String currentNetSalary;
+    private String employersEmail;
+    private String employersPhoneNumber;
+    private String districtAndPayrollCode;
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+    //Bank details
+    private String bankName;
+    private String otherBankName;
+    private String branchName;
+    private String accountName;
+    private String accountNo;
+    private String accountType;
+    private String otherAccountType;
 
-    public int getInstallment() {
-        return installment;
-    }
+    //Documents
+    private String nationalIdUpload;
+    private String documentNationalId;
+    private String documentNationalIdBase64;
 
-    public void setInstallment(int installment) {
-        this.installment = installment;
-    }
+    private String clientPictureUpload;
+    private String documentPhoto;
+    private String documentPhotoBase64;
 
-    public String getProduct() {
-        return product;
-    }
+    private String payslipUpload;
+    private String documentPayslip;
+    private String documentPayslipBase64;
 
-    public void setProduct(String product) {
-        this.product = product;
-    }
+    private String documentInvoice;
+    private String documentInvoiceBase64;
+    private String documentMarriageCertificate;
+    private String documentMarriageCertificateBase64;
 
-    public String getApplicationDate() {
-        return applicationDate;
-    }
+    private String proofOfEmployemntUpload;
+    private String documentProofOfEmployment;
+    private String documentProofOfEmploymentBase64;
 
-    public void setApplicationDate(String applicationDate) {
-        this.applicationDate = applicationDate;
-    }
+    private String marriageCertificateUpload;
+    private String mariage_cert;
+
+    private String serial_num;
+    private String serialNumberUpload;
+    private String documentSerialNumber;
+    private String documentSerialNumberBase64;
+
+    private String invoiceupload;
+    private String invoicepic;
+
+    //Next of Kin 1 Details
+    private String nxtOfKin1TitleGroup;
+    private String nxtOfKin1FirstName;
+    private String nxtOfKin1Surname;
+    private String nxtOfKin1ResidentialAddress;
+    private String nxtOfKin1PhoneNumber;
+    private String nxtOfKin1Relation;
+    private String nxtOfKin1NameOfEmployer;
+    private String nxtOfKin1EmployerAddress;
+
+    //Next of Kin 2 Details
+    private String nxtOfKin2TitleGroup;
+    private String nxtOfKin2FirstName;
+    private String nxtOfKin2Surname;
+    private String nxtOfKin2ResidentialAddress;
+    private String nxtOfKin2PhoneNumber;
+    private String nxtOfKin2Relation;
+    private String nxtOfKin2NameOfEmployer;
+    private String nxtOfKin2EmployerAddress;
+
+    //Declaration
+    private String borrowerFullName;
+    private String placeOfSignature;
+    private String borrowerSignature;
+    private String borrowerSignatureBase64;
+    private String dateSignBorrower;
+
+    private String witnessFullName;
+    private String witnessSignature;
+    private String witnessSignatureBase64;
+    private String dateSignWitness;
+    private String witnessPlaceOfSignature;
+
+    private String witnessFullName2;
+    private String witnessSignature2;
+    private String witness2SignatureBase64;
+    private String dateSignWitness2;
+    private String witnessPlaceOfSignature2;
+
+    //Office Use Details
+    private String authorisedBy;
+    private String authoriserSignature;
+    private String authorizerSignatureBase64;
+    private String officeStamp;
+    private String officeStampBase64;
+
+
 }
